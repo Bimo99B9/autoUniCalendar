d20b54b1825db18b2d01071cecad59a26b539a73
Bimo99B9 <52236615+Bimo99B9@users.noreply.github.com>
2021-09-11 15:26:12 +0200

    Major update.

--- autoUniCalendar.py
+++ autoUniCalendar.py
@@ -7,132 +7,146 @@ import sys
 import urllib.parse
 import os
 
+# Check if the required arguments have been provided, and indicate the use of the script.
 if len(sys.argv) != 3:
     print("\n[!] Uso: python3 " + sys.argv[0] + " <JSESSIONID> <RENDERMAP.TOKEN>\n")
     sys.exit(1)
 
+# Declare global variables.
+url = 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml'
 session = sys.argv[1]
-rendermap = sys.argv[2]
+render_map = sys.argv[2]
 
-print("[i] autoUniCalendar, a script to convert the Uniovi calendar to Google and Microsoft calendars.")
+# Script information.
+print("[i] autoUniCalendar, a script which converts the Uniovi calendar into Google and Microsoft calendars.")
 print("[i] Designed and programmed by Daniel López Gala from the University of Oviedo.")
 print("[i] Visit Bimo99B9.github.io for more content.\n")
-print("\n[*] The provided session is: " + session)
-print("[*] The provided render token is: " + rendermap + "\n")
+print(f"[*] The provided session is: {session}")
+print(f"[*] The provided render token is: {render_map}")
 
+# Function to send the first GET HTTP request using the tokens provided.
+def get_first_request(session_token, render_token):
 
-# print(session + rendermap)
-
-def get_first_request(s, r):
     print("[@] Sending the first request...")
-    url = 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml'
+
+    # Cookies payload of the HTTP request.
     payload = {
-        'JSESSIONID': s,
-        'oam.Flash.RENDERMAP.TOKEN': r
+        'JSESSIONID': session_token,
+        'oam.Flash.RENDERMAP.TOKEN': render_token
     }
+
     r = requests.get(url, cookies=payload)
     print("[#] First request correctly finished.\n")
+    # The function returns the server response to use it later.
     return r.text
 
-
-def extract_cookies(req):
+# Function to extract the cookies necessary to make the POST request, from the server response of the first request.
+def extract_cookies(get_response):
     print("[@] Extracting the calendar parameters...")
-    for line in req.split('\n'):
+
+    # Iterate the response lines to search the cookies, and save them in variables.
+    # To-do: Optimize search algorithm.
+    for line in get_response.split('\n'):
         if '<div id="j_id' in line:
-            tmp = line.split('<')
-            src = tmp[1]
-            src2 = re.findall('"([^"]*)"', src)[0]
-            source = urllib.parse.quote(src2)
-            print("[*] javax.faces.source retrieved: " + source)
+            source = urllib.parse.quote(re.findall('"([^"]*)"', line.split('<')[1])[0])
+            print(f"[*] javax.faces.source retrieved: {source}")
             break
 
-    for line in req.split('\n'):
+    for line in get_response.split('\n'):
         if 'javax.faces.ViewState' in line:
-            tmp = line
-            st = tmp.split(' ')[12]
-            viewst = re.findall('"([^"]*)"', st)[0]
-            viewstate = urllib.parse.quote(viewst)
-            print("[*] javax.faces.ViewState retrieved: " + viewstate)
+            viewstate = urllib.parse.quote(re.findall('"([^"]*)"', line.split(' ')[12])[0])
+            print(f"[*] javax.faces.ViewState retrieved: {viewstate}")
             break
 
-    for line in req.split('\n'):
+    for line in get_response.split('\n'):
         if 'action="/serviciosacademicos/web/expedientes/calendario.xhtml"' in line:
-            tmp = line.split(' ')
-            submit = re.findall('"([^"]*)"', tmp[3])[0]
-            print("[*] javax.faces.source_SUBMIT retrieved: " + submit)
+            submit = re.findall('"([^"]*)"', line.split(' ')[3])[0]
+            print(f"[*] javax.faces.source_SUBMIT retrieved: {submit}")
             break
 
     print("[#] Calendar parameters extracted.\n")
-
+    # The function returns a list that contains the extracted parameters.
     return [source, viewstate, submit]
 
+# Function that sends the HTTP POST request to the server and retrieves the raw data of the calendar.
+def post_second_request(session_token, render_token, ajax, source, view, start, end, submit):
 
-def post_second_request(s, r, ajax, source, view, start, end, submit):
     print("[@] Sending the calendar request...")
-
-    url = 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml'
+    
+    # Cookies of the request.
     payload = {
-        'JSESSIONID': s,
-        'oam.Flash.RENDERMAP.TOKEN': r,
+        'JSESSIONID': session_token,
+        'oam.Flash.RENDERMAP.TOKEN': render_token,
         'cookieconsent_status': 'dismiss'
     }
-    stringstart = source + "_start"
-    stringend = source + "_end"
-    stringsubmit = submit + "_SUBMIT"
-    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 
+    # Define variables of the request.
+    string_start = source + "_start"
+    string_end = source + "_end"
+    string_submit = submit + "_SUBMIT"
+
+    # Creating the body with the parameters extracted before, with the syntax required by the server.
     print("[*] Creating the payload...")
-    bodypayload = "javax.faces.partial.ajax=" + ajax + "&javax.faces.source=" + source + "&javax.faces.partial.execute=" + source + "&javax.faces.partial.render=" + source + "&" + source + "=" + source + "&" + stringstart + "=" + start + "&" + stringend + "=" + end + "&" + stringsubmit + "=1&javax.faces.ViewState=" + view
+    body_payload = f"javax.faces.partial.ajax={ajax}&javax.faces.source={source}&javax.faces.partial.execute={source}&javax.faces.partial.render={source}&{source}={source}&{string_start}={start}&{string_end}={end}&{string_submit}=1&javax.faces.ViewState={view}"
 
-    r = requests.post(url, data=bodypayload, headers=headers, cookies=payload)
+    # Send the POST request. 
+    r = requests.post(url, data=body_payload, headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, cookies=payload)
     print("[#] Calendar request correctly retrieved.\n")
 
+    # Write the raw response into a temporary file.
     print("[@] Writing the raw calendar data into a .txt file...")
     f = open("raw.txt", "w")
     f.write(r.text)
     f.close()
     print("[#] File correctly written.\n")
 
-
-def treatFile(file):
+# Function that creates a CSV file readable by the applications, from the raw data previously retrieved.
+def create_csv(file):
 
     print("[@] Creating the CSV file...")
+    
+    # Create the file.
     f = open(file, "r")
     g = open("Calendario.CSV", "w")
+
+    # Write the headers in the first line.
     g.write("Asunto,Fecha de comienzo,Comienzo,Fecha de finalización,Finalización,Todo el dí­a,Reminder on/off,Reminder Date,Reminder Time,Meeting Organizer,Required Attendees,Optional Attendees,Recursos de la reuniÃƒÂ³n,Billing Information,Categories,Description,Location,Mileage,Priority,Private,Sensitivity,Show time as\n")
-    data = f.read()
-    text = data.split('<')
-    calendar = text[5]
-    events = calendar.split('{')
+    
+    # Separate the events from its XML context.
+    text = f.read().split('<')
+    events = text[5].split('{')
     del events[0:2]
+
+    # Each field of the event is separated by commas.
     print("[*] Parsing the data...")
     for event in events:
-        res = []
-        for ele in event.split(','):
-            if ele.strip():
-                res.append(ele)
-        id = res[0]
-        title = res[1]
-        start = res[2]
-        end = res[3]
-        allday = res[4]
-        editable = res[5]
-        className = res[6]
-        description = res[7]
+        data = []
+        for field in event.split(','):
+            # Remove empty fields.
+            if field.strip():
+                data.append(field)
+        # Save in variables the fields needed to build the CSV line of the event.
+        title = data[1]
+        start = data[2]
+        end = data[3]
+        description = data[7]
         
-        titulo = re.findall('"([^"]*)"', title.split(':')[1])[0]
-        tmp = start.split(' ')[1].split('T')[0].removeprefix('"')
-        fechainicio = tmp.split('-')[2]+'/'+tmp.split('-')[1]+'/'+tmp.split('-')[0]
-        horainicio = start.split(' ')[1].split('T')[1].split('+')[0]
-        tmp = end.split(' ')[1].split('T')[0].removeprefix('"')
-        fechafin = tmp.split('-')[2]+'/'+tmp.split('-')[1]+'/'+tmp.split('-')[0]
-        horafin = end.split(' ')[1].split('T')[1].split('+')[0]
-        fechadealerta = fechainicio
-        horadealerta = str(int(start.split(' ')[1].split('T')[1].split('+')[0].split(':')[0]) - 1) + ':' + start.split(' ')[1].split('T')[1].split('+')[0].split(':')[1] + ':' + start.split(' ')[1].split('T')[1].split('+')[0].split(':')[2]
-        creador = "Universidad de Oviedo"
+        # Make the necessary strings transformations to adapts the raw field data into a CSV readable file.
+        title_csv = re.findall('"([^"]*)"', title.split(':')[1])[0]
+        start_date = start.split(' ')[1].split('T')[0].removeprefix('"')
+        start_date_csv = start_date.split('-')[2]+'/'+start_date.split('-')[1]+'/'+start_date.split('-')[0]
+        start_hour = start.split(' ')[1].split('T')[1].split('+')[0]
+        end_date = end.split(' ')[1].split('T')[0].removeprefix('"')
+        end_date_csv = end_date.split('-')[2]+'/'+end_date.split('-')[1]+'/'+end_date.split('-')[0]
+        end_hour = end.split(' ')[1].split('T')[1].split('+')[0]
+        alert_date = start_date_csv
+        alert_hour = str(int(start.split(' ')[1].split('T')[1].split('+')[0].split(':')[0]) - 1) + ':' + start.split(' ')[1].split('T')[1].split('+')[0].split(':')[1] + ':' + start.split(' ')[1].split('T')[1].split('+')[0].split(':')[2]
+        event_creator = "Universidad de Oviedo"
         body = description.split('"')[3].replace(r'\n', '')
-        csvline = titulo+','+fechainicio+','+horainicio+','+fechafin+','+horafin+',FALSO,FALSO,'+fechadealerta+','+horadealerta+','+creador+',,,,,,'+body+',,,Normal,Falso,Normal,2\n'
-        g.write(csvline)
+        # Write all the fields into a single line, and append it to the file.
+        csv_line = f"{title_csv},{start_date_csv},{start_hour},{end_date_csv},{end_hour},FALSO,FALSO,{alert_date},{alert_hour},{event_creator},,,,,,{body},,,Normal,Falso,Normal,2\n"
+        g.write(csv_line)
+        
     print("[*] Events correctly written in the CSV file.")
     f.close()
     g.close()
@@ -141,7 +155,7 @@ def treatFile(file):
     print("\n[#] Calendar generated. You can now import it in Outlook or Google Calendar selecting 'import from file' and providing the CSV file generated.\n")
 
 
-first_request = get_first_request(session, rendermap)
+first_request = get_first_request(session, render_map)
 cookies = extract_cookies(first_request)
-post_second_request(session, rendermap, "true", cookies[0], cookies[1], "1630886400000", "1652054400000", cookies[2])
-treatFile("raw.txt")
+post_second_request(session, render_map, "true", cookies[0], cookies[1], "1630886400000", "1652054400000", cookies[2])
+create_csv("raw.txt")
