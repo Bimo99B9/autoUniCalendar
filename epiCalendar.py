31d58713930d27a23a0f80b3e652d609739280a1
miermontoto <UO283319@uniovi.es>
2022-02-02 20:38:16 +0100

    console output, minor changes

--- epiCalendar.py
+++ epiCalendar.py
@@ -16,18 +16,16 @@ if len(sys.argv) != 3:
 url = 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml'
 session = sys.argv[1]
 render_map = sys.argv[2]
+reg = '"([^"]*)"'
+tmp = "epiTmpFile"
 
 # Script information.
-print("[i] autoUniCalendar, a script which converts the Uniovi calendar into Google and Microsoft calendars.")
-print("[i] Designed and programmed by Daniel López Gala from the University of Oviedo.")
-print("[i] Visit Bimo99B9.github.io for more content.\n")
-print(f"[*] The provided session cookie is: {session}")
-print(f"[*] The provided render token is: {render_map}")
+#print(f"Using {session} and {render_map} as cookies.")
 
 # Function to send the first GET HTTP request using the tokens provided.
 def get_first_request(session_token, render_token):
 
-    print("[@] Sending the first request...")
+    print("Sending the first request...", end=" ")
 
     # Cookies payload of the HTTP request.
     payload = {
@@ -36,38 +34,42 @@ def get_first_request(session_token, render_token):
     }
 
     r = requests.get(url, cookies=payload)
-    print("[#] First request correctly finished.\n")
+    print("✅")
     # The function returns the server response to use it later.
     return r.text
 
 # Function to extract the cookies necessary to make the POST request, from the server response of the first request.
 def extract_cookies(get_response):
-    print("[@] Extracting the calendar parameters...")
+    print("Extracting calendar parameters...", end=" ")
 
     # Iterate the response lines to search the cookies, and save them in variables.
 
     found_first, found_second, found_third = False, False, False
     for line in get_response.split('\n'):
         if '<div id="j_id' in line and not found_first:
-            source = urllib.parse.quote(re.findall('"([^"]*)"', line.split('<')[1])[0])
+            source = urllib.parse.quote(re.findall(reg, line.split('<')[1])[0])
             found_first = True
 
         if 'javax.faces.ViewState' in line and not found_second:
-            viewstate = urllib.parse.quote(re.findall('"([^"]*)"', line.split(' ')[12])[0])
+            viewstate = urllib.parse.quote(re.findall(reg, line.split(' ')[12])[0])
             found_second = True
 
         if 'action="/serviciosacademicos/web/expedientes/calendario.xhtml"' in line and not found_third:
-            submit = re.findall('"([^"]*)"', line.split(' ')[3])[0]
+            submit = re.findall(reg, line.split(' ')[3])[0]
             found_third = True
 
-    print("[#] Calendar parameters extracted.\n")
+    print("✅")
     # The function returns a list that contains the extracted parameters.
-    return [source, viewstate, submit]
+    try:
+        return [source, viewstate, submit]
+    except UnboundLocalError:
+        print("Couldn't extract calendar parameters. (¿Invalid cookies?)")
+        exit(1)
 
 # Function that sends the HTTP POST request to the server and retrieves the raw data of the calendar.
 def post_second_request(session_token, render_token, ajax, source, view, start, end, submit):
 
-    print("[@] Sending the calendar request...")
+    print("Sending the calendar request...", end=" ")
     
     # Cookies of the request.
     payload = {
@@ -82,31 +84,32 @@ def post_second_request(session_token, render_token, ajax, source, view, start, 
     string_submit = submit + "_SUBMIT"
 
     # Creating the body with the parameters extracted before, with the syntax required by the server.
-    print("[*] Creating the payload...")
     body_payload = f"javax.faces.partial.ajax={ajax}&javax.faces.source={source}&javax.faces.partial.execute={source}&javax.faces.partial.render={source}&{source}={source}&{string_start}={start}&{string_end}={end}&{string_submit}=1&javax.faces.ViewState={view}"
 
     # Send the POST request. 
     r = requests.post(url, data=body_payload, headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, cookies=payload)
-    print("[#] Calendar request correctly retrieved.\n")
+    print("✅")
 
     # Write the raw response into a temporary file.
-    print("[@] Writing the raw calendar data into a .txt file...")
-    f = open("raw.txt", "w")
+    print("Writing the raw calendar data to a temp file...", end=" ")
+    f = open(tmp, "w")
     f.write(r.text)
     f.close()
-    print("[#] File correctly written.\n")
+    print("✅")
 
 # Function that creates a CSV file readable by the applications, from the raw data previously retrieved.
 def create_csv(file):
 
-    print("[@] Creating the CSV file...")
+    print("Creating csv file...", end=" ")
     
     # Create the file.
     f = open(file, "r")
-    g = open("Calendario.CSV", "w")
+    g = open("Calendario.csv", "w")
+
+    print("✅")
 
     # Write the headers in the first line.
-    g.write("Asunto,Fecha de comienzo,Comienzo,Fecha de finalización,Finalización,Todo el dí­a,Reminder on/off,Reminder Date,Reminder Time,Meeting Organizer,Required Attendees,Optional Attendees,Recursos de la reuniÃƒÂ³n,Billing Information,Categories,Description,Location,Mileage,Priority,Private,Sensitivity,Show time as\n")
+    g.write("Asunto,Fecha de comienzo,Comienzo,Fecha de finalización,Finalización,Todo el día,Reminder on/off,Reminder Date,Reminder Time,Meeting Organizer,Required Attendees,Optional Attendees,Recursos de la reuniÃƒÂ³n,Billing Information,Categories,Description,Location,Mileage,Priority,Private,Sensitivity,Show time as\n")
     
     # Separate the events from its XML context.
     text = f.read().split('<')
@@ -114,7 +117,7 @@ def create_csv(file):
     del events[0:2]
 
     # Each field of the event is separated by commas.
-    print("[*] Parsing the data...")
+    print("Parsing the data...", end=" ")
     for event in events:
         data = []
         for field in event.split(','):
@@ -128,15 +131,15 @@ def create_csv(file):
         description = data[7]
         
         # Make the necessary strings transformations to adapts the raw field data into a CSV readable file.
-        title_csv = re.findall('"([^"]*)"', title.split(':')[1])[0]
+        title_csv = re.findall(reg, title.split(':')[1])[0]
 
         title = title_csv.split(" - ")[0]
-        type = title_csv.split(" - ")[1].replace('.','').replace('-', ' ').rsplit()
-        if type[0] == "Teoría": type = f"CEX"
-        elif type[1] == "Grupales": type = f"TG{type[2  ].strip('0')}"
-        elif type[2] == "Aula": type = f"PA{type[3].strip('0')}"
-        elif type[2] == "Laboratorio": type = f"PL{type[3].strip('0')}"
-        title_csv = f"{title} ({type})"
+        classType = title_csv.split(" - ")[1].replace('.','').replace('-', ' ').rsplit()
+        if classType[0] == "Teoría": classType = f"CEX"
+        elif classType[1] == "Grupales": classType = f"TG{classType[2  ].strip('0')}"
+        elif classType[2] == "Aula": classType = f"PA{classType[3].strip('0')}"
+        elif classType[2] == "Laboratorio": classType = f"PL{classType[3].strip('0')}"
+        title = f"{title} ({classType})"
 
         start_date = start.split(' ')[1].split('T')[0].split('"')[1]
         start_date_csv = start_date.split('-')[2]+'/'+start_date.split('-')[1]+'/'+start_date.split('-')[0]
@@ -162,18 +165,25 @@ def create_csv(file):
         elif location[0] == "Aula": location = f"AN-{location[1]}"
 
         # Write all the fields into a single line, and append it to the file.
-        csv_line = f"{title_csv},{start_date_csv},{start_hour},{end_date_csv},{end_hour},FALSO,FALSO,{alert_date},{alert_hour},{event_creator},,,,,,{info},{location},,Normal,Falso,Normal,2\n"
+        csv_line = f"{title},{start_date_csv},{start_hour},{end_date_csv},{end_hour},FALSO,FALSO,{alert_date},{alert_hour},{event_creator},,,,,,{info},{location},,Normal,Falso,Normal,2\n"
         g.write(csv_line)
         
-    print("[*] Events correctly written in the CSV file.")
+    print("✅")
     f.close()
     g.close()
-    print("[*] Removing raw .txt file...")
-    os.remove("raw.txt")
-    print("\n[#] Calendar generated. You can now import it in Outlook or Google Calendar selecting 'import from file' and providing the CSV file generated.\n")
+    print("Removing temp file...", end=" ")
+    os.remove(tmp)
+    print("✅")
+    print("\nCalendar generated.\n")
 
 
+<<<<<<< HEAD
 first_request = get_first_request(session, render_map)
 cookies = extract_cookies(first_request)
 post_second_request(session, render_map, "true", cookies[0], cookies[1], "1662444000000", "1683612000000", cookies[2])
 create_csv("raw.txt")
+=======
+cookies = extract_cookies(get_first_request(session, render_map))
+post_second_request(session, render_map, "true", cookies[0], cookies[1], "1630886400000", "1652054400000", cookies[2])
+create_csv(tmp)
+>>>>>>> d7a59862 (console output, minor changes)
