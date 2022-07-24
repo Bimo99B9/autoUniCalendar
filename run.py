7a3b69ac68968ed80afb9054cd8ff5ad35f32df1
miermontoto <underscoreBis@gmail.com>
2022-07-24 14:23:42 +0200

    unique filename for each reload to avoid downloading unknown files

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import epiCalendar
 import os
+import uuid
 from flask import Flask, render_template, request, send_file
 app = Flask(__name__)
 defaultFilename = epiCalendar.csvFile
+uuidStr = ""
 
 @app.route('/', methods = ['GET'])
 def index():
+    global uuidStr
+    uuidStr = str(uuid.uuid4())
     return render_template('index.html')
 
 @app.route('/', methods = ['POST'])
@@ -24,9 +28,12 @@ def form_post():
         if not 'class-type' in settings: argv.append('--disable-class-type-parsing')
         if not 'experimental-location' in settings: argv.append('--disable-experimental-location-parsing')
 
+        argv.append('-o')
+        argv.append(uuidStr)
+
         if epiCalendar.main(argv) == 0:
-            target = send_file(defaultFilename, as_attachment=True, attachment_filename=filename)
-            if os.path.exists(filename): os.remove(filename)
+            target = send_file(uuidStr, as_attachment=True, attachment_filename=filename)
+            os.remove(uuidStr)
             return target
 
     return render_template('index.html', slug_error='Something went wrong. Check your JSESSIONID and try again.')
