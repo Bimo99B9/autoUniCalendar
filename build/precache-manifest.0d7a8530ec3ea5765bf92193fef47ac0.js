593e126b07cb5430461a98b3ebaebdba0c7e1f05
Jonathan Arias Busto <UO283586@uniovi.es>
2022-07-25 00:38:44 +0200

    new build

---
+++ build/precache-manifest.0d7a8530ec3ea5765bf92193fef47ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b24f9afa15990f53fa",
+    "url": "/static/css/main.58ec83ea.chunk.css"
+  },
+  {
+    "revision": "54b24f9afa15990f53fa",
+    "url": "/static/js/main.3412ec9a.chunk.js"
+  },
+  {
+    "revision": "31b6faa5ac5540836e4f",
+    "url": "/static/js/runtime~main.bb5fe24d.js"
+  },
+  {
+    "revision": "81fad91babb9d3b2011c",
+    "url": "/static/js/2.b022bbe8.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "2f3db4afe29674fe9dae4108c5b2e881",
+    "url": "/index.html"
+  }
+];
