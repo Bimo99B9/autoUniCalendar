46bb1fcc6610a3d189429865dd85854272699878
miermontoto <underscoreBis@gmail.com>
2022-07-24 13:12:39 +0200

    basic cookie verification

--- static/js/form.js
+++ static/js/form.js
@@ -24,3 +24,22 @@ function toggleModes(radioUO, radioEPI, cbLocation, cbExperimentalLocation, cbCl
 function clearCookie(idCookie) {
     document.getElementById(idCookie).value = "";
 }
+
+function verifyCookie(idCookie, idSubmit) {
+    var cookieDom = document.getElementById(idCookie);
+    var submit = document.getElementById(idSubmit);
+    var cookie = cookieDom.value;
+
+    console.log(cookie.innerHTML);
+    if (cookie.length == 37) {
+        if (cookie.charAt(0) == '0' && cookie.charAt(1) == '0' && cookie.charAt(2) == '0' && cookie.charAt(3) == '0'
+            && cookie.charAt(27) == ':' && cookie.charAt(28) == '1' && cookie.charAt(29) == 'd') {
+            submit.disabled = false;
+            cookieDom.classList.add("text-input-correct");
+        }
+    } else {
+        submit.disabled = true;
+        cookieDom.classList.remove("text-input-correct");
+    }
+
+}
