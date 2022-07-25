7425a08819cecf3c7957e4e5f788333f7143e818
Jonathan Arias Busto <UO283586@uniovi.es>
2022-07-25 11:31:43 +0200

    settings radiobuttons fixed

--- src/components/Settings/Options.js
+++ src/components/Settings/Options.js
@@ -3,15 +3,19 @@ import Checkboxes from "./Checkboxes";
 
 import classes from "./Options.module.css";
 import RadioButtons from "./RadioButtons";
+import SettingsContext from "../../store/settings-context";
 
 const Options = () => {
+  const ctx = React.useContext(SettingsContext);
+
   return (
     <React.Fragment>
       <div className={classes.options}>
         <h2>Settings</h2>
       </div>
-      <RadioButtons />
+      <RadioButtons onClick={ctx.check} />
       <Checkboxes />
+      <p>{ctx.university}</p>
     </React.Fragment>
   );
 };
--- src/store/settings-context.js
+++ src/store/settings-context.js
@@ -1,48 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 
-const SettingsContext = React.createContext({
-  isEPI: true,
-  isUO: false,
+const SettingsContext = createContext({
   check: (name) => {},
+  university: "",
 });
 
 export const SettingsProvider = (props) => {
-  const [isEPI, setIsEPI] = useState(true);
-  const [isUO, setIsUO] = useState(false);
+  const [university, setUniversity] = useState("epi");
 
   const checkHandler = (name) => {
+    setUniversity(name);
     console.log(name);
-    if (name.trim() === "uo") {
-      if (isUO === false && isEPI === true) {
-        setIsEPI(false);
-        setIsUO(true);
-      } else if (isUO === true && isEPI === false) {
-        setIsEPI(false);
-        setIsUO(false);
-      } else if (isUO === false && isEPI === false) {
-        setIsEPI(true);
-        setIsUO(false);
-      }
-    } else if (name.trim() === "epi") {
-      if (isEPI === false && isUO === true) {
-        setIsEPI(true);
-        setIsUO(false);
-      } else if (isEPI === true && isUO === false) {
-        setIsEPI(false);
-        setIsUO(false);
-      } else if (isEPI === false && isUO === false) {
-        setIsEPI(true);
-        setIsUO(false);
-      }
-    }
   };
 
   return (
     <SettingsContext.Provider
       value={{
-        isEpi: isEPI,
-        isUO: isUO,
         check: checkHandler,
+        university: university,
       }}
     >
       {props.children}
