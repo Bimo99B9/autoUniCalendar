26b8154ff0356d3180e14e376aee9af3dbe1131f
miermontoto <underscoreBis@gmail.com>
2022-08-14 13:32:16 +0200

    support iCalendar format (default), other fixes

--- epiCalendar.py
+++ epiCalendar.py
@@ -15,7 +15,7 @@ from datetime import datetime
 # Declare global variables.
 url = 'https://sies.uniovi.es/serviciosacademicos/web/expedientes/calendario.xhtml'
 reg = '"([^"]*)"'
-csvFile = "Calendario.csv" # Can be changed through "-o" flag.
+filename = "Calendario" # Can be changed through "-o" flag.
 rSession = requests.Session()
 
 # Toggle location and class type parsing using the following global variables.
@@ -24,7 +24,10 @@ rSession = requests.Session()
 # This options can be easily toggled through argument flags.
 enableLocationParsing = True
 enableClassTypeParsing = True
+enableLinks = True
 enableStatistics = False
+icsMode = True
+
 
 
 # Function to send the first GET request using the cookie provided.
@@ -103,19 +106,21 @@ def postCalendarRequest(jsessionid, cookies):
         print("× (Invalid response)")
         exit(1)
 
-    sampleId = re.search(r'[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}', result).group(0)
-    locationPayload = f"javax.faces.partial.ajax=true&javax.faces.source={source}&javax.faces.partial.execute={source}&javax.faces.partial.render={source[:10:]}eventDetails+{source[:10:]}aulas_url&javax.faces.behaviour.event=eventSelect&javax.faces.partial.event=eventSelect&{source}_selectedEventId={sampleId}&{submit}_SUBMIT=1&javax.faces.ViewState={view}"
-
-    locationInfo = postRequest(locationPayload, payload).text
-    removeCharacters = ['\t', '\n', 'class="enlaceUniovi"', '</li>', '</a>', '<a href=', 'target="_blank">' , '"']
-    for char in removeCharacters:
-        locationInfo = locationInfo.replace(char, '')
-
-    locationInfo = locationInfo.split('<li>')[1:]
-    locationInfo[-1] = locationInfo[-1].split('</ul>')[0]
     locations = {}
-    for location in locationInfo:
-        locations[location.split('  ')[1].lower()] = location.split('  ')[0]
+    if enableLinks:
+        sampleId = re.search(r'[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}', result).group(0)
+        locationPayload = f"javax.faces.partial.ajax=true&javax.faces.source={source}&javax.faces.partial.execute={source}&javax.faces.partial.render={source[:10:]}eventDetails+{source[:10:]}aulas_url&javax.faces.behaviour.event=eventSelect&javax.faces.partial.event=eventSelect&{source}_selectedEventId={sampleId}&{submit}_SUBMIT=1&javax.faces.ViewState={view}"
+
+        locationInfo = postRequest(locationPayload, payload).text
+        removeCharacters = ['\t', '\n', 'class="enlaceUniovi"', '</li>', '</a>', '<a href=', 'target="_blank">' , '"']
+        for char in removeCharacters:
+            locationInfo = locationInfo.replace(char, '')
+
+        locationInfo = locationInfo.split('<li>')[1:]
+        locationInfo[-1] = locationInfo[-1].split('</ul>')[0]
+
+        for location in locationInfo:
+            locations[location.split('  ')[1].lower()] = location.split('  ')[0]
 
     print("✓ (%.3fs)" % (time.time() - initTime))
     return result, locations
@@ -177,20 +182,20 @@ def parseLocation(loc):
 def parseClassType(type):
 
     if not enableClassTypeParsing: return type
-    classGroup = type.replace('.','').replace('-', ' ').rsplit()[-1].strip('0').upper()
-    lang = "🇬🇧" if "inglés" in type.lower() else ""
+    typeL = type.lower()
+    classGroup = type.replace('-', ' ').rsplit()[-1].strip('0').upper()
+    lang = "🇬🇧" if "inglés" in typeL else ""
 
-    if "Teoría" in type: return f"CEX{lang}"
-    if "Tutoría" in type or "Grupal" in type: return f"TG{classGroup}"
-    if "Aula" in type: return f"PA{classGroup}"
-    if "Laboratorio": return f"PL{classGroup}"
+    if "teoría" in typeL: return f"CEX{lang}"
+    if "tutoría" in typeL or "grupal" in typeL: return f"TG{classGroup}"
+    if "laboratorio" in typeL: return f"PL{classGroup}"
+    if "aula" in typeL: return f"PA{classGroup}"
 
     return type # If the class type is not recognized, return the original string.
 
-# Function that creates a CSV file readable by the applications, from the raw data previously retrieved.
-def createCsv(rawResponse, locations):
+def generateCalendar(rawResponse, locations):
 
-    print("Parsing data and generating new csv...", end=" ", flush=True)
+    print("Parsing data and creating calendar...", end=" ", flush=True)
     initTime = time.time()
 
     stats = {
@@ -200,15 +205,16 @@ def createCsv(rawResponse, locations):
         "locations": {}
     }
 
-    g = open(csvFile, "w")
-
-    # Write the headers in the first line.
-    g.write("Subject,Start Date,Start Time,End Date,End Time,Description,Location,Organizer\n")
-
     # Separate the events from its XML context.
     text = rawResponse.split('<')
     events = text[5].split('{')
     del events[0:2]
+
+    if icsMode:
+        c = Calendar()
+    else:
+        g = open(filename + ".csv", "w")
+        g.write("Subject,Start Date,Start Time,End Date,End Time,Description,Location\n")
 
     # Each field of the event is separated by commas.
     for event in events:
@@ -218,45 +224,51 @@ def createCsv(rawResponse, locations):
             if field.strip():
                 data.append(field)
         # Save in variables the fields needed to build the CSV line of the event.
-        title = data[1]
-        start = data[2]
-        end = data[3]
-        description = data[7]
+        uid = data[0].split(': ')[1].replace('"', '')
+        title = data[1].split(': ')[1].replace('"', '')
+        start = data[2].split(': ')[1].replace('T', ' ').replace('"', '').split('+')[0]
+        end = data[3].split(': ')[1].replace('T', ' ').replace('"', '').split('+')[0]
+        description = data[7].split('"')[3].replace(r'\n', '')
 
-        # Make the necessary strings transformations to adapts the raw field data into a CSV readable file.
-        title_csv = re.findall(reg, title.split(':')[1])[0]
-
-        titleSplit = title_csv.split(" - ")
+        titleSplit = title.split(" - ")
         classType = parseClassType(titleSplit[1])
         title = f"{titleSplit[0]} ({classType})"
 
-        start_date = start.split(' ')[1].split('T')[0].split('"')[1]
-        start_hour = start.split(' ')[1].split('T')[1].split('+')[0]
-        end_date = end.split(' ')[1].split('T')[0].split('"')[1]
-        end_hour = end.split(' ')[1].split('T')[1].split('+')[0]
+        start_date = start.split(' ')[0]
+        start_hour = start.split(' ')[1]
+        end_date = end.split(' ')[0]
+        end_hour = end.split(' ')[1]
 
-        body = description.split('"')[3].replace(r'\n', '')
-        loc = body.split(" - ")[1]
+        loc = description.split(" - ")[1]
         location = parseLocation(loc)
-        link = locations[loc.lower()]
-
-        # Write all the fields into a single line, and append it to the file.
-        csv_line = f"{title},{start_date},{start_hour},{end_date},{end_hour},{body} ({link}),{location},Universidad de Oviedo\n"
-        g.write(csv_line)
+        if enableLinks: description += f" ({locations[loc.lower()]})"
 
         # Update the statistics.
-        stats["classes"] += 1
-        stats["hours"] += int(end_hour.split(':')[0]) - int(start_hour.split(':')[0])
-        if classType not in stats["classTypes"]:
-            stats["classTypes"][classType] = 1
-        else:
-            stats["classTypes"][classType] += 1
-        if location not in stats["locations"]:
-            stats["locations"][location] = 1
-        else:
-            stats["locations"][location] += 1
+        if enableStatistics:
+            stats["classes"] += 1
+            stats["hours"] += int(end_hour.split(':')[0]) - int(start_hour.split(':')[0])
+            if classType not in stats["classTypes"]:
+                stats["classTypes"][classType] = 1
+            else:
+                stats["classTypes"][classType] += 1
+            if location not in stats["locations"]:
+                stats["locations"][location] = 1
+            else:
+                stats["locations"][location] += 1
 
-    g.close()
+        if icsMode:
+            e = Event(name=title, begin=start, end=end, description=description, location=location, uid=uid)
+            c.events.add(e)
+        else:
+            csv_line = f"{title},{start_date},{start_hour},{end_date},{end_hour},{description},{location}\n"
+            g.write(csv_line)
+
+    if icsMode:
+        with open(filename + ".ics", "w") as f:
+            f.writelines(c.serialize_iter())
+    else:
+        g.close()
+
     print("✓ (%.3fs)" % (time.time() - initTime))
 
     # Sort the class types and locations by number of occurrences.
@@ -265,20 +277,21 @@ def createCsv(rawResponse, locations):
 
     return stats
 
-
 def main(argv) -> int:
-    global enableLocationParsing, enableClassTypeParsing, enableStatistics, csvFile
+    global enableLocationParsing, enableClassTypeParsing, enableStatistics, filename, icsMode, enableLinks
     session = ""
 
     # Read flags from arguments.
-    if not len(argv) == 1 and (argv[1] == "--help" or argv[1] == "-h"):
+    if "--help" in argv or "-h" in argv:
         print("Usage: python3 epiCalendar.py [JSESSIONID] [-o | --output-file <filename>] [--disable-location-parsing] [--disable-class-type-parsing]")
-        exit(0)
+        return 0
 
     for i in range(1, len(argv)):
         if argv[i] == "--disable-location-parsing": enableLocationParsing = False
         if argv[i] == "--disable-class-type-parsing": enableClassTypeParsing = False
-        if argv[i] == "-o" or argv[i] == "--output-file" : csvFile = argv[i+1]
+        if argv[i] == "--disable-links": enableLinks = False
+        if argv[i] == "--csv": icsMode = False
+        if argv[i] == "-o" or argv[i] == "--output-file" : filename = argv[i+1]
         if argv[i] == "-s" or argv[i] == "--stats" or argv[i] == "--enable-statistics": enableStatistics = True
         if utils.verifyCookieStructure(argv[i]): session = argv[i]
 
@@ -287,7 +300,7 @@ def main(argv) -> int:
         try:
             session = input("Enter JSESSIONID: ")
         except (KeyboardInterrupt, EOFError):
-            exit(0)
+            return 0
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -317,8 +330,12 @@ create_csv(tmp)
     # If the JSESSIONID is not valid, exit.
     if not utils.verifyCookieStructure(session):
         print("× Invalid JSESSIONID.")
+<<<<<<< HEAD
         exit(1)
 >>>>>>> 8d8cb67e (it works!)
+=======
+        return 1
+>>>>>>> c9a6a02f (support iCalendar format (default), other fixes)
 
     startTime = time.time()
 <<<<<<< HEAD
@@ -338,6 +355,7 @@ create_csv(tmp)
 =======
     rawResponse, locations = postCalendarRequest(session, cookies)
 <<<<<<< HEAD
+<<<<<<< HEAD
     stats = createCsv(rawResponse)
 >>>>>>> 2b1fc7ee (obtain links for each location , english parsing, other minor changes)
 =======
@@ -353,6 +371,12 @@ create_csv(tmp)
 <<<<<<< HEAD
 >>>>>>> dea54aa3 (f/b comms, no settings and no download)
 =======
+=======
+    stats = generateCalendar(rawResponse, locations)
+    print("\nCalendar generated, took %.3fs" % (time.time() - startTime))
+    ext = "ics" if icsMode else "csv"
+    print(f"Saved as \"{filename}.{ext}\"")
+>>>>>>> c9a6a02f (support iCalendar format (default), other fixes)
 
     if enableStatistics:
         print("\nStatistics:")
