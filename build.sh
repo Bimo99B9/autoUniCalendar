f93929fc80d55c853dc7c9c44005f98a045f80cb
miermontoto <underscoreBis@gmail.com>
2022-09-05 13:21:40 +0200

    adapt branch to upstream repo

--- build.sh
+++
@@ -1,2 +0,0 @@
-npm run linux-build
-sed -i 's/<div id="overlays">/<div id="overlays"><span id="error">{{ slug }}<\/span>/g' build/index.html
--- run.py
+++ run.py
@@ -1,16 +1,15 @@
-import epiCalendar
+import autoUniCalendar
 import os
 import uuid
 import utils
 import re
 
-
 from flask import Flask, render_template, request, send_file
 from flask_talisman import Talisman
-app = Flask(__name__, static_folder='./build', static_url_path='/', template_folder='./build')
+app = Flask(__name__)
 Talisman(app, content_security_policy=None)
 
-defaultFilename = epiCalendar.filename
+defaultFilename = autoUniCalendar.filename
 debug = os.environ.get('FLASK_ENV') == 'development'
 
 @app.route('/', methods = ['GET'])
@@ -19,13 +18,13 @@ def index():
 
 @app.route('/', methods = ['POST'])
 def form_post():
-    if debug: print(f"[DEBUG] POST data received from React: {request.form}")
+    if debug: print(f"[DEBUG] POST data received: {request.form}")
 
-    jsessionid = request.form['jsessionid']
-    filename = request.form['filename']
-    location = request.form['location'] == "true"
-    classType = request.form['class-type'] == "true"
-    extension = request.form['extension']
+    jsessionid = request.form.get('cookie1')
+    filename = defaultFilename
+    location = True
+    classType = True
+    extension = ".ics"
 
     if debug:
         print(f"[DEBUG] Calendar info: {jsessionid} → {filename}{extension}")
@@ -35,7 +34,7 @@ def form_post():
 
     if utils.verifyCookieExpiration(jsessionid):
 
-        argv = ['epiCalendar.py', jsessionid]
+        argv = ['autoUniCalendar.py', jsessionid]
 
         if not location: argv.append('--disable-location-parsing')
         if not classType: argv.append('--disable-class-type-parsing')
@@ -52,7 +51,7 @@ def form_post():
             print(f"[DEBUG] UUID: {uuidStr}")
             print(f"[DEBUG] Arguments: {argv}")
 
-        exitCode = epiCalendar.main(argv)
+        exitCode = autoUniCalendar.main(argv)
         if os.path.exists(backendFilename) and exitCode == 0:
             if debug: print(f"[DEBUG] Attempting to serve {backendFilename} as {downloadFilename}.")
             target = send_file(backendFilename, as_attachment=True, attachment_filename=downloadFilename)
@@ -61,16 +60,17 @@ def form_post():
             return target
         elif exitCode == 2:
             if debug: print("[DEBUG] [ERROR] ¿No calendar events?")
-            return serve(slug="ERROR: No hay eventos en el calendario.")
+            #return serve(slug="ERROR: No hay eventos en el calendario.")
         if debug: print("[DEBUG] [ERROR] Script failed to generate file.")
-        return serve("ERROR: No se pudo generar el calendario.")
+        #return serve("ERROR: No se pudo generar el calendario.")
 
     elif debug:
         print("[DEBUG] [ERROR] Expired cookie submited.")
 
-    return serve(slug="ERROR: cookie inválida.")
+    #return serve(slug="ERROR: cookie inválida.")
+    return serve()
 
 
 @app.errorhandler(404)
-def serve(slug=""):
-    return render_template('index.html', slug=slug)
+def serve():
+    return render_template('index.html')
