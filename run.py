3160decb86c43f3821b19d0915ba7998b100c02e
miermontoto <underscoreBis@gmail.com>
2022-09-07 12:49:17 +0200

    adapt backend to upstream

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import re
 
 from flask import Flask, render_template, request, send_file
 from flask_talisman import Talisman
-app = Flask(__name__, static_folder='./build', static_url_path='/', template_folder='./build')
+app = Flask(__name__)
 Talisman(app, content_security_policy=None)
 
 defaultFilename = epiCalendar.filename
@@ -21,11 +21,11 @@ def index():
 def form_post():
     if debug: print(f"[DEBUG] POST data received from React: {request.form}")
 
-    jsessionid = request.form['jsessionid']
-    filename = request.form['filename']
-    location = request.form['location'] == "true"
-    classType = request.form['class-type'] == "true"
-    extension = request.form['extension']
+    jsessionid = request.form.get('cookie1')
+    filename = defaultFilename
+    location = True
+    classType = True
+    extension = ".ics"
 
     if debug:
         print(f"[DEBUG] Calendar info: {jsessionid} → {filename}{extension}")
@@ -61,14 +61,15 @@ def form_post():
             return target
         elif exitCode == 2:
             if debug: print("[DEBUG] [ERROR] ¿No calendar events?")
-            return serve(slug="ERROR: No hay eventos en el calendario.")
+            #return serve(slug="ERROR: No hay eventos en el calendario.")
         if debug: print("[DEBUG] [ERROR] Script failed to generate file.")
-        return serve("ERROR: No se pudo generar el calendario.")
+        #return serve("ERROR: No se pudo generar el calendario.")
 
     elif debug:
         print("[DEBUG] [ERROR] Expired cookie submited.")
 
-    return serve(slug="ERROR: cookie inválida.")
+    #return serve(slug="ERROR: cookie inválida.")
+    return serve()
 
 
 @app.errorhandler(404)
